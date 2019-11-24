90e6922360558b7c139d0cab8cbf46311f351254
emreozincegedik <emreozincegedik@gmail.com>
2019-11-24 20:18:00 +0300

    predict full folder instead of single image

--- predict.py
+++ predict.py
@@ -3,36 +3,63 @@ import numpy as np
 import keras
 import matplotlib.pyplot as plt
 import cv2
-model=keras.models.load_model('dog_cat_2_10.h5')
+from tqdm import tqdm
+import os
+model=keras.models.load_model('models/dog_cat_2_10.h5')
 
-# img_array = cv2.imread("test/a (5).jfif" ,cv2.IMREAD_GRAYSCALE)  # convert to array
-# new_array = cv2.resize(img_array, (100, 100))
-# img=np.array(new_array)
-# img=np.expand_dims(img, axis=0)
-# img=np.expand_dims(img, axis=-1)
-# # img=np.moveaxis(img,0,-1)
+IMG_SIZE=100
+DATADIR = "test"
 
-# print(img.shape)
-# print(img)
+CATEGORIES = ["Dog", "Cat"]
 
-img = cv2.imread("test/download.jfif",cv2.IMREAD_GRAYSCALE)
-img = cv2.resize(img,(100,100))
-img = np.reshape(img,[1,100,100,1])
+def create_testing_data():
+    for category in CATEGORIES:  # dogs cats
 
-classes = model.predict_classes(img)
+        path = os.path.join(DATADIR,category)  # create path to dogs and cats
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+            try:
+                file_list.append(os.path.join(path,img))
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, category])  # add this to our training_data
+            except Exception as e:  # in the interest in keeping the output clean...
+                print(e)
+            #except OSError as e:
+            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
+            #except Exception as e:
+            #    print("general exception", e, os.path.join(path,img))
+
+training_data = []
+
+file_list=[]
+create_testing_data()
+
+X = []
+y = []
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+X = np.array(X).reshape(-1,1, IMG_SIZE, IMG_SIZE, 1)
+y = np.array(y)
+
+X=X/255 #0-255 to 0-1
+print(file_list)
+from tabulate import tabulate
+array=[]
+
+classes = model.predict_classes(X[0])
+array.append([classes,y[0],file_list[0]])
+for i in range(len(file_list)):
+  classes = model.predict_classes(X[i])
+  if classes[0][0]==1:
+    prediction="cat"
+  else:
+    prediction="dog"
+  array.append([prediction,y[i],file_list[i]])
+  
+print(tabulate(array, headers=['prediction', 'reality','file name']))
 
 
 
-
-# img_pred=image.load_img('test/a (9).jfif', target_size=(100,100),color_mode='grayscale')
-# img_pred=np.array(img_pred)
-# img_pred=np.expand_dims(img_pred, axis=0)
-# print(img_pred.shape)
-# images = np.vstack([img_pred])
-# print(images.shape)
-# classes = model.predict_classes(images, batch_size=10)
-# print(classes)
-if classes[0][0]==1:
-  print("cat")
-else:
-  print("dog")
