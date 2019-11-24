ded9fa9c0a38092526e1ff3d8cd6d09326882070
emreozincegedik <emreozincegedik@gmail.com>
2019-11-24 20:24:20 +0300

    option to make full crossvalidation model with one go

--- train.py
+++ train.py
@@ -12,18 +12,20 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 
+train_all_blocks_at_same_time=False #If false, you can specify which specific block to train, otherwise it will train all blocks
 IMG_SIZE=100
 batch_size=30 #image processing number at the same time, needed for model_fit_generator
 epochs=10 #training number
 training_multiplier=100 #train epochs*x time and save model x time
 blocksize=10 #cross validation block size
-block_number=7 #cross validation block number 
+block_number=10 #cross validation block number 
 model_file='dog_cat'
 model_file_name=model_file+f'_{block_number}_{blocksize}.h5'
 images_folder="images"
 images_file="X.pickle"
 images_label="y.pickle"
 models_folder="models"
+training_categories= ["Dog", "Cat"] # must be the training images folder names
 
 def validation_block(X,y,block_size=10,validation_block_number=10):
   if validation_block_number>block_size or validation_block_number<1:
@@ -46,7 +48,7 @@ def validation_block(X,y,block_size=10,validation_block_number=10):
   return ((x_train, y_train),(x_test,y_test))
 
 def create_training_data():
-    for category in CATEGORIES:  # dogs flowers
+    for category in CATEGORIES:  # dogs cats
 
         path = os.path.join(DATADIR,category)  # create path to dogs and cats
         class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=flower
@@ -63,6 +65,37 @@ def create_training_data():
             #except Exception as e:
             #    print("general exception", e, os.path.join(path,img))
 
+def create_model():
+  if K.image_data_format() == 'channels_first':
+      input_shape = (1, IMG_SIZE, IMG_SIZE)
+  else:
+      input_shape = (IMG_SIZE, IMG_SIZE, 1)
+
+  model = Sequential()
+  model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+  model.add(Activation('relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+
+  model.add(Conv2D(32, (3, 3)))
+  model.add(Activation('relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+
+  model.add(Conv2D(64, (3, 3)))
+  model.add(Activation('relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+
+  model.add(Flatten())
+  model.add(Dense(64))
+  model.add(Activation('relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(1))
+  model.add(Activation('sigmoid'))
+
+  model.compile(loss='binary_crossentropy',
+                optimizer='rmsprop',
+                metrics=['accuracy'])
+
+  return model
 
 if os.path.exists(images_file) and os.path.exists(images_label):
   pickle_in = open(images_file,"rb")
@@ -73,7 +106,7 @@ if os.path.exists(images_file) and os.path.exists(images_label):
 else:
   DATADIR = images_folder
 
-  CATEGORIES = ["Dog", "Cat"]
+  CATEGORIES = training_categories
 
   training_data = []
 
@@ -107,48 +140,40 @@ else:
 
 if os.path.exists(os.path.join(models_folder, model_file_name)):
   model=load_model(os.path.join(models_folder, model_file_name))
-else:
-  if K.image_data_format() == 'channels_first':
-      input_shape = (1, IMG_SIZE, IMG_SIZE)
-  else:
-      input_shape = (IMG_SIZE, IMG_SIZE, 1)
+else: 
+  model=create_model()
+  """
+    if K.image_data_format() == 'channels_first':
+        input_shape = (1, IMG_SIZE, IMG_SIZE)
+    else:
+        input_shape = (IMG_SIZE, IMG_SIZE, 1)
 
-  model = Sequential()
-  model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2)))
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-  model.add(Conv2D(32, (3, 3)))
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-  model.add(Conv2D(64, (3, 3)))
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-  model.add(Flatten())
-  model.add(Dense(64))
-  model.add(Activation('relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(1))
-  model.add(Activation('sigmoid'))
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
 
-  model.compile(loss='binary_crossentropy',
-                optimizer='rmsprop',
-                metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])"""
 
-# # model.fit_generator() needs this. If you need more training data use this method and comment model.fit()
-# train_datagen = ImageDataGenerator(
-#     # rescale=1.1,
-#     # rescale=1. / 255,
-#     shear_range=0.2,
-#     zoom_range=0.2,
-#     horizontal_flip=True,
-#     rotation_range=20,
-#     width_shift_range=0.2,
-#     height_shift_range=0.2)
-  
-(x_train,y_train),(x_test,y_test)=validation_block(X,y,blocksize,block_number)
+
+""" (x_train,y_train),(x_test,y_test)=validation_block(X,y,blocksize,block_number)
 for i in range(training_multiplier):
   print(f"{i+1}/{training_multiplier}. training")
 
@@ -161,4 +186,46 @@ for i in range(training_multiplier):
   #   epochs=epochs
   #   )
 
-  model.save(os.path.join(models_folder, model_file_name))
+  model.save(os.path.join(models_folder, model_file_name)) """
+
+# # model.fit_generator() needs this. If you need more training data use this method and comment model.fit()
+# train_datagen = ImageDataGenerator(
+#     # rescale=1.1,
+#     # rescale=1. / 255,
+#     shear_range=0.2,
+#     zoom_range=0.2,
+#     horizontal_flip=True,
+#     rotation_range=20,
+#     width_shift_range=0.2,
+#     height_shift_range=0.2)
+
+if train_all_blocks_at_same_time:
+  evaluations=[]
+  for i in range(blocksize):
+    print(f"Training {i+1}. block of {blocksize} blocks")
+
+    (x_train,y_train),(x_test,y_test)=validation_block(X,y,(i+1),block_number)
+    model=create_model()
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs)
+    # # Use this instead of model.fit if you need more training data
+    # model.fit_generator(
+    #   train_datagen.flow(x_train, y_train, batch_size=batch_size),
+    #   validation_data=(x_test, y_test), 
+    #   steps_per_epoch=len(x_train) // batch_size,
+    #   epochs=epochs
+    #   ) 
+    model.save(os.path.join(models_folder, model_file_name))
+else:
+  (x_train,y_train),(x_test,y_test)=validation_block(X,y,blocksize,block_number)
+  for i in range(training_multiplier):
+    print(f"{i+1}/{training_multiplier}. training of {block_number}. block")
+
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=epochs)
+    # # Use this instead of model.fit if you need more training data
+    # model.fit_generator(
+    #   train_datagen.flow(x_train, y_train, batch_size=batch_size),
+    #   validation_data=(x_test, y_test), 
+    #   steps_per_epoch=len(x_train) // batch_size,
+    #   epochs=epochs
+    #   ) 
+    model.save(os.path.join(models_folder, model_file_name))
